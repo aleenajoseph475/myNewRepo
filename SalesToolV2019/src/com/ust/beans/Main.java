ccf870b5a2e8e8034822b8a26075c9f498dbc46b
aleen_000 <aleen_000@Admin>
2019-09-24 14:53:13 +0530

    initial commit by aleena

---
+++ SalesToolV2019/src/com/ust/beans/Main.java
@@ -0,0 +1,19 @@
+package com.ust.beans;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		SalesData sales=new SalesData();
+		System.out.println("Welcome to sales App");
+		displayGreetings();
+		sales.display();
+	}
+	
+	public static void displayGreetings()
+	{
+		System.out.println("Sales report 2019");
+	}
+	
+
+}
---
+++ SalesToolV2019/src/com/ust/beans/SalesData.java
@@ -0,0 +1,10 @@
+package com.ust.beans;
+
+public class SalesData {
+	
+	public static void display()
+	{
+		System.out.println("Sales 2019");
+	}
+
+}
